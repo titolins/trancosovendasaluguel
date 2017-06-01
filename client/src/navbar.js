334879dfc0dd147da74830c9022c6ab007aac1e9
Tito Lins <tito@blinx.com.br>
2017-06-01 00:29:52 -0300

    improved and added link to todos

--- client/src/navbar.js
+++ client/src/navbar.js
@@ -1,10 +1,19 @@
 import React from 'react'
 
+import { Link } from 'react-router-dom'
+
 export default class Navbar extends React.Component {
   render() {
     return (
-      <nav className="navbar navbar-toggeable-md navbar-inverse bg-inverse mb-4">
-        <a href="#" className="navbar-brand">GoReact Starter</a>
+      <nav className="navbar navbar-toggleable-md navbar-inverse bg-inverse mb-4">
+        <Link to="/" className="navbar-brand">GoReact Starter</Link>
+        <div className="collapse navbar-collapse">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item">
+              <Link className="nav-link" to="/todos">todos</Link>
+            </li>
+          </ul>
+        </div>
       </nav>
     )
   }
