1517ea93d8e8acb188557a27aee9916f68aaef5c
Tito Lins <tito@blinx.com.br>
2017-06-02 19:54:54 -0300

    added actions

---
+++ client/src/actions.js
@@ -0,0 +1,36 @@
+/**
+ * action types
+ */
+
+//export const SET_CONFIG = 'SET_CONFIG';
+
+// translation stuff
+export const CHOOSE_LANGUAGE = 'CHOOSE_LANG';
+
+export const ADD_CONTENT = 'ADD_CONTENT';
+
+
+/**
+ * action creators
+ */
+
+/*
+export function setConfig(config) {
+  return { type: SET_CONFIG, config };
+}
+*/
+
+// translation
+export function selectLanguage(lang) {
+  return {
+    type: CHOOSE_LANGUAGE,
+    lang
+  }
+}
+
+export function addContent(content) {
+  return {
+    type: ADD_CONTENT,
+    content
+  }
+}
