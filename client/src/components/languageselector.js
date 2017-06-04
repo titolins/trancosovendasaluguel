10162bba662b7e3db14bcf7f56a900c705d02481
Tito Lins <tito@blinx.com.br>
2017-06-04 01:22:11 -0300

    removed h5 element and active class for now

--- client/src/components/languageselector.js
+++ client/src/components/languageselector.js
@@ -4,14 +4,14 @@ import { PropTypes } from 'react';
 //import { activeLanguage } from './styles';
 
 export default ({ active, lang, onClick }) => {
-  let activeClass = active ? "active" : undefined;
+  //let activeClass = active ? "active" : undefined;
 
   return (
-      <span><a className={activeClass} href="#" onClick={e => {
+      <span><a className="nav-link" href="#" onClick={e => {
         e.preventDefault();
         onClick();
       }}
-      ><h5>{ lang }</h5></a></span>
+      >{ lang }</a></span>
   )
 }
 
