6aa9cb59ff35ae12d2e23c8ef9d5e6e00e934ea2
Tito Lins <tito@blinx.com.br>
2017-06-06 01:36:25 -0300

    removed commented code

--- client/src/reducers.js
+++ client/src/reducers.js
@@ -9,17 +9,6 @@ const initialState = {
   content: Content
 }
 
-/*
-function config(state = {}, action) {
-  switch (action.type) {
-  case SET_CONFIG:
-    return action.config;
-  default:
-    return state;
-  }
-}
-*/
-
 function lang(state = initialState, action) {
   switch (action.type) {
     case CHOOSE_LANGUAGE:
@@ -31,52 +20,11 @@ function lang(state = initialState, action) {
   }
 }
 
-/*
-// 1st step
-// we first get the relevant content of the items, where:
-// result[0] = portuguese content
-// result[1] = english content
-function extractContent(content) {
-  content.map((item) => {
-    return [ item.Content.PT,
-             item.Content.EN ]
-  })
-}
-
- *
- * result of the above:
- *
- * ```
- *  [
- *    [item_0[PT],item_0[EN]],
- *    [item_1[PT],item_1[EN]]
- *  ]
- * ```
-
-// 2nd step
-// now we need to transform the result above into language arrays
-function filterContent(content) {
-  return { "PT_BR": extractedContent.filter((item) => { return item[0] }),
-           "EN_US": extractedContent.filter((item) => { return item[1] }) }
-}
-
-// wrapper
-function parseContent(content) {
-  console.log("parsed content")
-  console.log(filterContent(extractContent(content)))
-}
- */
-
 function content(state = initialState, action) {
   switch (action.type) {
     case ADD_CONTENT:
-      console.log("state")
-      console.log(state)
-      console.log("action")
-      console.log(action)
       let contentState = {}
       contentState[action.contentId] = action.content
-
       return Object.assign({}, state.content, { dinamic: Object.assign({}, state.content.dinamic, contentState) })
     default:
       return state
@@ -84,7 +32,6 @@ function content(state = initialState, action) {
 }
 
 export default combineReducers({
-  //config,
   lang,
   content
 });
