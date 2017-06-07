2154f32a1bf2c8b3782bb00a2ab9790ddb485010
Tito Lins <tito@blinx.com.br>
2017-06-07 18:07:23 -0300

    using bootstrap columns for building logo

--- client/src/components/logo.js
+++ client/src/components/logo.js
@@ -6,11 +6,11 @@ export default class Logo extends React.Component {
   render() {
     return (
       <Link to="/">
-        <div className="logo">
-          <div className="logoCol">
+        <div className="row no-gutters align-items-center logo">
+          <div className="col-3">
             <object data="/static/img/logo.svg" type="image/svg+xml" />
           </div>
-          <div className="logoCol logoText">
+          <div className="col-9 logoText">
             <p>Trancoso</p>
             <p>vendas e aluguel</p>
           </div>
