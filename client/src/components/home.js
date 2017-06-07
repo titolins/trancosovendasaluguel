f36982df759f09c463a6354a66a75281312fff55
Tito Lins <tito@blinx.com.br>
2017-06-07 18:07:48 -0300

    started using home

--- client/src/components/home.js
+++ client/src/components/home.js
@@ -6,7 +6,24 @@ export default class Home extends React.Component {
   render() {
     return (
       <div className="home">
-        <img className="mainPicture" src="/static/img/main_photo.png"></img>
+        <div className="welcome">
+          <div className="row">
+            <div className="col-xs-12">
+              <img className="mainPicture" src="/static/img/main_photo.png"></img>
+            </div>
+          </div>
+          <div className="container">
+            <div className="row">
+              <div className="col-xs-12 col-md-5">
+                <p className="">
+                </p>
+              </div>
+              <div className="col-xs-12 col-md-7">
+                <img className="welcomePicture" src="/static/img/welcome_picture.png"></img>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
