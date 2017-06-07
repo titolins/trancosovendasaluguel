ba5b1dea878d71870b79ef0b375ece155883ce55
Tito Lins <tito@blinx.com.br>
2017-06-07 11:54:25 -0300

    added logotext

--- client/src/components/logo.js
+++ client/src/components/logo.js
@@ -5,11 +5,17 @@ import { Link } from 'react-router-dom'
 export default class Logo extends React.Component {
   render() {
     return (
-      <div>
-        <Link to="/">
-          <object data="/static/img/logo.svg" type="image/svg+xml" />
-        </Link>
-      </div>
+      <Link to="/">
+        <div className="logo">
+          <div className="logoCol">
+            <object data="/static/img/logo.svg" type="image/svg+xml" />
+          </div>
+          <div className="logoCol logoText">
+            <p>Trancoso</p>
+            <p>vendas e aluguel</p>
+          </div>
+        </div>
+      </Link>
     )
   }
 }
