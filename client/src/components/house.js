f20b00c1c2372545b0886d7fed5982ebb5cafb9d
Tito Lins <tito@blinx.com.br>
2017-06-10 01:44:59 -0300

    house container working

--- client/src/components/house.js
+++ client/src/components/house.js
@@ -3,6 +3,7 @@ import React from 'react'
 import { connect } from 'react-redux'
 
 import { mapDispatchToProps } from 'client/containers/fetchcontent'
+import { mapStateToProps } from 'client/containers/house'
 
 class House extends React.Component {
   constructor(props) {
@@ -14,13 +15,22 @@ class House extends React.Component {
   }
 
   render() {
+    let { ownContent } = this.props
     return (
-      <div>
-        <h1>Teste</h1>
+      <div className="container mainContent">
+        <div className="row">
+          <div className="col-xs-12 col-md-7 houseCarousel">
+          </div>
+          <div className="col-xs-12 col-md-5">
+            <div>{ ownContent.Name }</div>
+            <div>{ ownContent.Description }</div>
+            <div>{ ownContent.Capacity }</div>
+          </div>
+        </div>
       </div>
     )
   }
 
 }
 
-export default connect(null, mapDispatchToProps)(House)
+export default connect(mapStateToProps, mapDispatchToProps)(House)
