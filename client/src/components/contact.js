97e3a3eeba3971b5425c557b219b23c0e4ada31f
Tito Lins <tito@blinx.com.br>
2017-06-12 11:08:59 -0300

    finished rough contact page

--- client/src/components/contact.js
+++ client/src/components/contact.js
@@ -3,32 +3,34 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { mapStateToProps } from 'client/containers/staticcontent'
 
+import './styles/contact.less'
+
 class Contact extends React.Component {
   render() {
     let { ownContent } = this.props
     return (
-      <div className="row mainContent">
+      <div className="row mainContent py-4">
         <div className="col-xs-12 col-md-4">
-          <h1 className="sectionTitle">tamara flier</h1>
-          <h2 className="sectionTitle">creci-ba 12.950</h2>
+          <h1 className="contactName">tamara flier</h1>
+          <h2 className="creciTitle">creci-ba 12.950</h2>
           <ul>
-            <h3 className="sectionTitle">{ ownContent.phone }</h3>
-            <li>+55 73 3668-1095</li>
-            <li>+55 73 98129-0825</li>
+            <h3 className="contactTitle">{ ownContent.phone }</h3>
+            <li className="ml-5 contactText">+55 73 3668-1095</li>
+            <li className="ml-5 contactText">+55 73 98129-0825</li>
           </ul>
           <ul>
-            <h3 className="sectionTitle">e-mail</h3>
-            <li>contact@trancosovendasaluguel.com</li>
-            <li>tamitrancoso@hotmail.com</li>
-            <li>tamitrancoso@gmail.com</li>
+            <h3 className="contactTitle">e-mail</h3>
+            <li className="ml-5 contactText">contact@trancosovendasaluguel.com</li>
+            <li className="ml-5 contactText">tamitrancoso@hotmail.com</li>
+            <li className="ml-5 contactText">tamitrancoso@gmail.com</li>
           </ul>
           <ul>
-            <h3 className="sectionTitle">skype</h3>
-            <li>tamara.flier</li>
+            <h3 className="contactTitle">skype</h3>
+            <li className="ml-5 contactText">tamara.flier</li>
           </ul>
           <ul>
-            <h3 className="sectionTitle">{ ownContent.address }</h3>
-            <p>
+            <h3 className="contactTitle">{ ownContent.address }</h3>
+            <p className="ml-4 contactText">
               Rua do Porto, n<sup>o</sup> 229, bairro João Vieira<br/>
               Trancoso/Bahia - CEP 22418-000<br/>
               Brasil
