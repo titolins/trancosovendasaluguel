2c51e62e11b9ca4d40655e520b23f249f726496d
Tito Lins <tito@blinx.com.br>
2017-06-25 00:15:11 -0300

    added some info to intro

--- client/admin/components/intro.js
+++ client/admin/components/intro.js
@@ -3,8 +3,11 @@ import React from 'react'
 export default class Intro extends React.Component {
   render() {
     return (
-      <div>
-        <h1>Intro</h1>
+      <div className="card">
+        <div className="card-block">
+          <h4 className="card-title">Bem-vindo ao Painel de Administração</h4>
+          <p className="card-text">Através do menu acima você poderá administrar o conteúdo do site, adicionando e editando novas casas e imagens, ou apenas visualizando-os.</p>
+        </div>
       </div>
     )
   }
