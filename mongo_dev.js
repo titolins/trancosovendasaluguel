17c9c158a3fd85d59286aec6e9bf848644fcf0f3
Tito Lins <tito@blinx.com.br>
2017-06-27 09:41:22 -0300

    added type property

--- mongo_dev.js
+++ mongo_dev.js
@@ -6,29 +6,31 @@ function buildHouse(c) {
 }
 
 var house = {
-    category: {
-      id: 1
-    },
-    featured: true,
-    capacity: 4,
-    cover: { url: "https://placehold.it/550x338" },
-    pictures: [
-      { url: "https://placehold.it/550x338" },
-      { url: "https://placehold.it/550x338" }
-    ],
-    content: {
-      'pt_br': {
-          name: "casa 1",
-          description: "descricao da casa",
+  category: {
+    id: 1
+  },
+  type: 'house', // only available on houses for sale
+                 // can be: house, lot, farm, store, inn
+  featured: true,
+  capacity: 4,
+  cover: { url: "https://placehold.it/550x338" },
+  pictures: [
+    { url: "https://placehold.it/550x338" },
+    { url: "https://placehold.it/550x338" }
+  ],
+  content: {
+    'pt_br': {
+        name: "casa 1",
+        description: "descricao da casa",
+        features: [
+            "wifi",
+            "ar condicionado" ]},
+      'en_us': {
+          name: "house 1",
+          description: "house description",
           features: [
-              "wifi",
-              "ar condicionado" ]},
-        'en_us': {
-            name: "house 1",
-            description: "house description",
-            features: [
-              "wifi",
-              "air conditioning" ]}}}
+            "wifi",
+            "air conditioning" ]}}}
 
 
 var salesCategory = {
