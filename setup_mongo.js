72216ad845096b74e4dc3b3f4e404b74a8389a4a
Tito Lins <tito@blinx.com.br>
2017-06-27 04:41:15 -0300

    added mongo script for initial setup

---
+++ setup_mongo.js
@@ -0,0 +1,34 @@
+
+function getHandler() {
+  conn = new Mongo();
+  return conn.getDB("tva");
+}
+
+// title is an object here, like so:
+// { pt_br: "",
+//   en_us: "" }
+function buildCategory(title) {
+  return {
+    content: {
+      "pt_br": { title: title["pt_br"] },
+      "en_us": { title: title["en_us"] }
+    },
+    houses: [],
+    featured: []
+  }
+}
+
+function main() {
+  var db = getHandler();
+  [
+    {
+      "pt_br": "vendas",
+      "en_us": "sales"
+    },
+    {
+      "pt_br": "aluguel",
+      "en_us": "rent"
+    }].map(function(c) { db.categories.insert(buildCategory(c)) });
+}
+
+main();
