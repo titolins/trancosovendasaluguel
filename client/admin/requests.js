36ccb414899c23ed448010cb11a701c15cf49cb8
Tito Lins <tito@blinx.com.br>
2017-06-29 10:14:43 -0300

    created default method for put requests

--- client/admin/requests.js
+++ client/admin/requests.js
@@ -8,6 +8,20 @@ export function getContentReq(url, token, callback){
   fetch(url, content).then(res=>res.json()).then(res=>callback(res))
 }
 
+export function putContentReq(url, token, data, callback) {
+  console.log(data)
+  let content = {
+    method: 'put',
+    headers: {
+      'Authorization': `Bearer ${token}`,
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  }
+  fetch(url, content).then(res=>res.json()).then(callback)
+}
+
 export function postContentReq(url, token, data, callback) {
   let content = {
     method: 'post',
