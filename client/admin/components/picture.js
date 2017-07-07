49f5989361d7fc7df4d637007ce1112c890f2493
Tito Lins <tito@blinx.com.br>
2017-07-07 11:47:36 -0300

    fixed modal not defined and removed col

--- client/admin/components/picture.js
+++ client/admin/components/picture.js
@@ -1,17 +1,16 @@
 import React from 'react'
 
+import Modal from 'client/admin/components/modal'
 
 export default class Picture extends React.Component {
   render() {
     return (
-      <div className="col-xs-12 col-md-6">
-        <div className="card">
-          <a href="#" data-toggle="modal" data-target={`#${this.props.pictureId}Modal`}>
-            <img className="card-img-top img-fluid" src={this.props.url} />
-          </a>
-          <div className="card-block">
-            <button type="button" onClick={this.props.handleDelete} className="btn btn-danger">Deletar</button>
-          </div>
+      <div className="card">
+        <a href="#" data-toggle="modal" data-target={`#${this.props.pictureId}Modal`}>
+          <img className="card-img-top img-fluid" src={this.props.url} />
+        </a>
+        <div className="card-block">
+          <button type="button" onClick={this.props.handleDelete} className="btn btn-danger">Deletar</button>
         </div>
         <Modal id={`${this.props.pictureId}Modal`}>
           <img className="img-fluid" src={this.props.url} />
