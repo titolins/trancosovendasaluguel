bd998d1459451e6466a111ad20baa8429d679aee
Tito Lins <tito@blinx.com.br>
2017-07-07 11:50:10 -0300

    added folder api endpoints. adapted and improved picture api

--- server/admin/api.go
+++ server/admin/api.go
@@ -40,6 +40,9 @@ func (api *API) Bind(group *echo.Group) {
     group.GET("/house", api.getAllHouses)
     group.PUT("/house", api.createHouse)
     group.DELETE("/house", api.deleteHouse)
+
+    group.GET("/folder", api.getAllFolders)
+    group.PUT("/folder", api.createFolder)
 }
 
 func (api *API) deleteHouse(c echo.Context) (err error) {
@@ -82,6 +85,19 @@ func (api *API) deletePicture(c echo.Context) (err error) {
     }
 
     return os.Remove(path.Join("server",p.Url))
+}
+
+func (api *API) getAllFolders(c echo.Context) (err error) {
+    var fs []models.PictureFolder
+
+    db := api.DB.Clone()
+    defer db.Close()
+
+    if err = db.DB("tva").C("folders").Find(bson.M{}).All(&fs); err != nil {
+        return
+    }
+
+    return c.JSON(200, fs)
 }
 
 func (api *API) getAllHouses(c echo.Context) (err error) {
@@ -194,48 +210,158 @@ func (api *API) uploadPictures(c echo.Context) (err error) {
     if form, err = c.MultipartForm(); err != nil {
         return
     }
+    log.Printf("%s", form)
+    log.Printf("%s", form.Value["folderId"][0])
+
+    /*
+    res := map[string][]string{
+        "errors": []string{} }
+    */
+    var errors []string
+
+    if len(form.File["pictures"]) < 1 {
+        errors = append(errors, "Nenhum arquivo para subir")
+    } else {
+        var validPictures []models.Picture
+        for _, file := range form.File["pictures"] {
+            func() (fErr error) {
+                // Source
+                src, fErr := file.Open()
+                if fErr != nil {
+                    errors = append(errors, fmt.Sprintf("Erro ao tentar abrir arquivo '%s'", file.Filename))
+                    return
+                }
+                defer src.Close()
+
+                if !ACCEPTED_EXT[strings.ToLower(path.Ext(file.Filename))] {
+                    errors = append(errors, fmt.Sprintf("Extensão '%s' inválida ('%s')", path.Ext(file.Filename), file.Filename))
+                    return
+                }
+
+                filepath := fmt.Sprintf("/srv/http/server/static/uploads/%s", file.Filename)
+                dst, fErr := os.Create(filepath)
+                if fErr != nil {
+                    errors = append(errors, fmt.Sprintf("Erro ao tentar criar arquivo '%s'", file.Filename))
+                    return
+                }
+                defer dst.Close()
+
+                // Copy
+                if _, fErr = io.Copy(dst, src); err != nil {
+                    errors = append(errors, fmt.Sprintf("Erro ao tentar copiar arquivo '%s'", file.Filename))
+                    return
+                }
+
+                url := fmt.Sprintf("/static/uploads/%s", file.Filename)
+                validPictures = append(validPictures, models.Picture{ Url: url})
+                /*
+                if fErr = db.DB("tva").C("pictures").Insert(&models.Picture{ Url: url }); err != nil {
+                    errors = append(errors, fmt.Sprintf("Erro ao tentar inserir arquivo '%s' no banco de dados", file.Filename))
+                }
+                */
+                return
+            }()
+
+        }
+
+        db := api.DB.Clone()
+        defer db.Close()
+
+        if bson.IsObjectIdHex(form.Value["folderId"][0]) {
+            if err = db.DB("tva").C("folders").UpdateId(bson.ObjectIdHex(form.Value["folderId"][0]), &bson.M{
+                "$push": &bson.M{ "pictures": &bson.M{
+                    "$each": validPictures}}}); err != nil {
+                errors = append(errors, "Erro ao inserir imagens no banco de dados")
+            }
+        } else {
+            errors = append(errors, "Id da pasta inválido")
+        }
+    }
+
+    if len(errors) > 0 {
+        return c.JSON(500, map[string]interface{}{ "error": true, "errors": errors })
+    }
+
+    return c.JSON(200, map[string]bool{ "error": false })
+}
+
+func (api *API) createFolder(c echo.Context) (err error) {
+    var f models.PictureFolder
+    errors := make(map[string]string)
+
+    if err = c.Bind(&f); err != nil {
+        log.Printf("%s", err)
+        return
+    }
+
+    log.Printf("f:\n%s", f)
+    log.Printf("f.Name:\n%s", f.Name)
+    log.Printf("len(f.Name):\n%d", len(f.Name))
+
+    if len(f.Name) < 4 {
+        log.Printf("thats it")
+        errors["name"] = "Nome da pasta deve ter ao menos 4 caractéres"
+        return c.JSON(500, map[string]interface{}{ "error": true, "errors": errors})
+    }
+
+    if strings.IndexFunc(f.Name, func(r rune) bool {
+        return r == '\\' ||
+            r == '\'' ||
+            r == '´' ||
+            r == '`' ||
+            r == ';' ||
+            r == ':' ||
+            r == '>' ||
+            r == '<' ||
+            r == ',' ||
+            r == '.' ||
+            r == '-' ||
+            r == '=' ||
+            r == '+' ||
+            r == '"' ||
+            r == '%' ||
+            r == '$' ||
+            r == '#' ||
+            r == '@' ||
+            r == '!' ||
+            r == '*' ||
+            r == '(' ||
+            r == ')' ||
+            r == '[' ||
+            r == '{' ||
+            r == '}' ||
+            r == ']' ||
+            r == '^' ||
+            r == '~' ||
+            r == '&'
+    }) != -1 {
+        errors["name"] = "Nome da pasta não pode conter os caractéres especiais '\\'´`;:><,.-=+'\"!@#$%&*()[]~^"
+        return c.JSON(500, map[string]interface{}{ "error": true, "errors": errors })
+    }
+
+    /* No need to actually mkdir
+    if err = os.Mkdir(path.Join("/srv/http/server/static/uploads",f.Name),os.ModeDir); err != nil {
+        if os.IsExist(err) {
+            errors["create"] = fmt.Sprintf("Pasta com nome '%s' já existe.", f.Name)
+        } else {
+            errors["create"] = fmt.Sprintf("Erro ao tentar criar diretório:\n%s", err)
+        }
+        return c.JSON(500, map[string]interface{}{ "error": true, "errors": errors })
+    }
+    */
 
     db := api.DB.Clone()
     defer db.Close()
-    res := map[string][]string{
-        "errors": []string{} }
-    for _, file := range form.File["pictures"] {
-        func() (fErr error) {
-            // Source
-            src, fErr := file.Open()
-            if fErr != nil {
-                res["errors"] = append(res["errors"], fmt.Sprintf("Erro ao tentar abrir arquivo '%s'", file.Filename))
-                return
-            }
-            defer src.Close()
-
-            if !ACCEPTED_EXT[strings.ToLower(path.Ext(file.Filename))] {
-                res["errors"] = append(res["errors"], fmt.Sprintf("Extensão '%s' inválida ('%s')", path.Ext(file.Filename), file.Filename))
-                return
-            }
-
-            filepath := fmt.Sprintf("/srv/http/server/static/uploads/%s", file.Filename)
-            dst, fErr := os.Create(filepath)
-            if fErr != nil {
-                res["errors"] = append(res["errors"], fmt.Sprintf("Erro ao tentar criar arquivo '%s'", file.Filename))
-                return
-            }
-            defer dst.Close()
-
-            // Copy
-            if _, fErr = io.Copy(dst, src); err != nil {
-                res["errors"] = append(res["errors"], fmt.Sprintf("Erro ao tentar copiar arquivo '%s'", file.Filename))
-                return
-            }
-
-            url := fmt.Sprintf("/static/uploads/%s", file.Filename)
-            if fErr = db.DB("tva").C("pictures").Insert(&models.Picture{ Url: url }); err != nil {
-                res["errors"] = append(res["errors"], fmt.Sprintf("Erro ao tentar inserir arquivo '%s' no banco de dados", file.Filename))
-            }
-            return
-        }()
-
+    if err = db.DB("tva").C("folders").Insert(&f); err != nil {
+        // check if error is a duplicate key error, in which case the folder
+        // already exists
+        if mgo.IsDup(err) {
+            errors["create"] = fmt.Sprintf("Pasta com nome '%s' já existe.", f.Name)
+        } else {
+            errors["create"] = fmt.Sprintf("Erro ao tentar inserir pasta no banco de dados:\n%s", err)
+        }
+        return c.JSON(500, map[string]interface{}{ "error": true, "errors": errors })
     }
-    return c.JSON(200, res )
-}
 
+    return c.JSON(200, map[string]bool{"error":false})
+}
