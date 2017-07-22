d35e9e3069d3a6697bccd9b82ab2d767b355c97c
Tito Lins <tito@blinx.com.br>
2017-07-22 10:53:52 -0300

    added cover photo as first in pictures and fixed house url

--- client/homepage/containers/category.js
+++ client/homepage/containers/category.js
@@ -1,32 +1,32 @@
 import { mapStateToProps as staticPropsMap } from 'homepage/containers/staticcontent'
 import { createFetchContentHandler } from 'homepage/containers/fetchcontent'
 
-export const filterItems = (items, lang) => {
-  if (!items) return null
-  return items.map((item) => {
+export const filterItems = (items, category, lang) => {
+  return items.map((item, i) => {
+    let pictures = item.pictureFolder.pictures || []
+    pictures.unshift(item.pictureFolder.cover)
     return {
       cover: item.pictureFolder.cover,
       name: item.name,
       content: item.content[lang],
-      pictures: item.pictureFolder.pictures,
-      //categoryId: item.category.name,
+      pictures: pictures,
       id: item.id,
-      url: `/categorias/${item.categories[0]}/casas/${item.id}`
+      url: `/categorias/${category}/casas/${item.id}`
     }
   })
 }
 
 export const mapStateToProps = (state, ownProps) => {
-  if (state.content.dinamic) window.items = filterItems(state.content.dinamic.category, state.lang.selected)
   try {
     let category = state.content.dinamic.categories[ownProps.categoryId],
         lang = state.lang.selected,
         items
     if (ownProps.categoryContent === 'category') items = state.content.dinamic.category
-    else { items = (category[ownProps.categoryContent].length > 0) ? category[ownProps.categoryContent] : category.items }
+    else if (category[ownProps.categoryContent].length > 0) items = category[ownProps.categoryContent]
+    else items = category.items
     return {
       title: category ? category.content[lang].title : null,
-      items: items ? filterItems(items, lang) : null,
+      items: items ? filterItems(items, ownProps.categoryId, lang) : null,
       ownContent: ownProps.contentId ? staticPropsMap(state, ownProps).ownContent : undefined,
       categoryTypes: state.content.static.categoryTypes[lang],
       categoryId: ownProps.categoryId
