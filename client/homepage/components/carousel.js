7dc18a00689ce48092ec935cf5040ac53b2554dd
Tito Lins <tito@blinx.com.br>
2017-07-22 12:16:14 -0300

    added picture modal

--- client/homepage/components/carousel.js
+++ client/homepage/components/carousel.js
@@ -4,34 +4,60 @@ import './styles/carousel.less'
 
 const buildIndicator = (item, i) => {
   let active = (i === 0) ? true : false
-  return (<li key={i} className={`list-inline-item${active ? " active" : ""}`}>
+  return (
+    <li key={i} className={`list-inline-item${active ? " active" : ""}`}>
       <a id={`selector-${i}`} className={active ? "selected" : "" } data-slide-to={i} data-target="#carousel">
         <img className="img-fluid" src={item.url} />
       </a>
-    </li>)
+    </li>
+  )
 }
 
 const buildPicture = (p, i) => {
   let active = (i === 0) ? "active" : null
   return (
     <div key={i} className={`carousel-item ${active}`}>
-      <img className="d-block img-fluid maxHeight" src={p.url}></img>
+      <a href="#" data-toggle="modal" data-target={`#imageModal${i}`}>
+        <img className="d-block img-fluid maxHeight" src={p.url}></img>
+      </a>
+    </div>
+  )
+}
+
+const buildModal = (p, i) => {
+  return (
+    <div key={i} className="modal fade" id={`imageModal${i}`} aria-hidden="true" tabIndex="-1" role="dialog">
+      <div className="modal-dialog modal-lg" role="document">
+        <div className="modal-content">
+          <div className="modal-header">
+            <button type="button" className="close" data-dismiss="modal" aria-label="Fechar">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div className="modal-body">
+            <img className="img-fluid" src={p.url}></img>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
 
 export default ({ pictures, controls }) => {
   let ps = [],
-      is = []
+      is = [],
+      ms = []
 
   pictures.map((p, i) => {
     ps.push(buildPicture(p, i))
     is.push(buildIndicator(p, i))
+    ms.push(buildModal(p, i))
   })
   return (
     <div id="carousel" className="carousel slide">
       <div className="carousel-inner" role="listbox">{ps}</div>
       <ul className="carousel-indicators list-inline">{is}</ul>
+      {ms}
     </div>
   )
 }
