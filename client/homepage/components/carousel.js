9e20e136a1592a8dc1f30bde153d2618d450ccb2
Tito Lins <tito@blinx.com.br>
2017-07-22 11:48:32 -0300

    refactored carousel creation (we were iterating twice)

--- client/homepage/components/carousel.js
+++ client/homepage/components/carousel.js
@@ -2,43 +2,36 @@ import React from 'react'
 
 import './styles/carousel.less'
 
-const buildIndicators = (items) => {
-  return (
-    <ul className="carousel-indicators list-inline">
-      { items.map((item, i) => {
-        let active = (i === 0) ? true : false
-        return (
-          <li key={i} className={`list-inline-item${active ? " active" : ""}`}>
-            <a id={`selector-${i}`} className={active ? "selected" : "" } data-slide-to={i} data-target="#carousel">
-              <img className="img-fluid" src={item.url} />
-            </a>
-          </li>
-        )
-      }) }
-    </ul>
-  )
+const buildIndicator = (item, i) => {
+  let active = (i === 0) ? true : false
+  return (<li key={i} className={`list-inline-item${active ? " active" : ""}`}>
+      <a id={`selector-${i}`} className={active ? "selected" : "" } data-slide-to={i} data-target="#carousel">
+        <img className="img-fluid" src={item.url} />
+      </a>
+    </li>)
 }
 
-const buildPictures = (pictures) => {
+const buildPicture = (p, i) => {
+  let active = (i === 0) ? "active" : null
   return (
-    <div className="carousel-inner" role="listbox">
-      { pictures.map((p, i) => {
-        let active = (i === 0) ? "active" : null
-        return (
-          <div key={i} className={`carousel-item ${active}`}>
-            <img className="d-block img-fluid maxHeight" src={p.url}></img>
-          </div>
-        )
-      }) }
+    <div key={i} className={`carousel-item ${active}`}>
+      <img className="d-block img-fluid maxHeight" src={p.url}></img>
     </div>
   )
 }
 
 export default ({ pictures, controls }) => {
+  let ps = [],
+      is = []
+
+  pictures.map((p, i) => {
+    ps.push(buildPicture(p, i))
+    is.push(buildIndicator(p, i))
+  })
   return (
     <div id="carousel" className="carousel slide">
-      { buildPictures(pictures) }
-      { buildIndicators(pictures) }
+      <div className="carousel-inner" role="listbox">{ps}</div>
+      <ul className="carousel-indicators list-inline">{is}</ul>
     </div>
   )
 }
