d25ffbd2fd2832bb419b9acd6d517b16b3c7d0fb
Tito Lins <tito@blinx.com.br>
2018-02-18 14:25:41 -0300

    serving static files from echo again

--- server/server.go
+++ server/server.go
@@ -51,11 +51,9 @@ func BuildEngine() (e *echo.Echo) {
     //e.Use(middleware.HTTPSRedirect())
 
     // static files
-    /*
     e.Static("/static/img", "/srv/http/server/static/img")
     e.Static("/static/js", "/srv/http/client/")
     e.Static("/static/uploads", "/srv/http/server/static/uploads")
-    */
 
     // api routes
     api := &API{ DB: db }
